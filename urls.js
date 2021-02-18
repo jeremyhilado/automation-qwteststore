28fd79f8a4ff846de7712c82aedff293b89aeabd
Jeremy Hilado <jhilado89@gmail.com>
2021-02-18 11:06:22 -0800

    added urls

---
+++ urls.js
@@ -0,0 +1,4 @@
+module.exports = {
+    prd: 'https://qw-test-store-prod.netlify.app/',
+    dev: 'https://qw-test-store-dev.netlify.app/'
+}
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,10 @@
+const url = require('./urls')
+const ENV = process.env.ENV
+
+if(!ENV || !['prd', 'dev'].includes(ENV)) {
+    console.log('Please use the following format when runnig the test sript: ENV=prd||dev')
+}
+
 exports.config = {
     //
     // ====================
@@ -90,7 +97,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://qw-test-store-prod.netlify.app/',
+    baseUrl: url[process.env.ENV],
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
